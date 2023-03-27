d88f18cdd9e96d964178411d357c2a9bfee0f715
Leandriguess <leandro.rodrigues.silva99@gmail.com>
2023-03-27 20:38:58 -0300

    Add initial files

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="header">O próximo feriado nacional é...</div>
+    <div class="holiday">{{ formattedDate }} - {{ formattedTitle }}</div>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      holiday: {
+        date: '2022-01-01',
+        title: 'Corpus Christi'
+      },
+    }
+  },
+  computed: {
+    formattedTitle() {
+      return this.holiday.title
+    },
+    formattedDate() {
+      return this.formattedDay + ' de ' + this.formattedMonth
+    },
+    formattedDay() {
+      const day = this.holiday.date.split('-')[2]
+      return day;
+    },
+    formattedMonth() {
+      const months = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro"
+      ]
+      const month = this.holiday.date.split('-')[1]
+
+      return months[parseInt(month) - 1]
+    }
   }
 }
+
 </script>
 
 <style>
@@ -21,6 +62,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  height: 100%;
+  justify-content: center;
+}
+
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+}
+
+.header {
+  font-size: 1.3rem;
+}
+
+.holiday {
+  font-size: 4rem;
 }
 </style>
