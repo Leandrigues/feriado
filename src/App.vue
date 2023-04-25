271e105f048107c51e977900ac52b757a584e779
Leandriguess <leandro.rodrigues.silva99@gmail.com>
2023-04-25 10:32:57 -0300

    Fix weekday

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,78 @@
 <template>
-  <div class="background" :style="{background: `linear-gradient(70deg, ${palettes[selectedColor].primary} 0%, ${palettes[selectedColor].secondary} 70%)`, color: palettes[selectedColor].text }">
+  <div
+    class="background"
+    :style="{
+      background: `linear-gradient(70deg, ${palettes[selectedColor].primary} 0%, ${palettes[selectedColor].secondary} 70%)`,
+      color: palettes[selectedColor].text,
+    }"
+  >
     <div class="container">
-      <div class="header">O próximo feriado nacional é <strong class="week-day">sexta-feira</strong>...</div>
-      <div class="holiday" v-if="nextHoliday">{{ formattedDate }} - {{ formattedTitle }}</div>
+      <div class="header">
+        O próximo feriado nacional é
+        <strong class="week-day">{{ formattedWeekday }}</strong
+        >...
+      </div>
+      <div class="holiday" v-if="nextHoliday">
+        {{ formattedDate }} - {{ formattedTitle }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import json from './data/holidays.json'
-import axios from 'axios'
+import json from "./data/holidays.json";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-  },
+  name: "App",
+  components: {},
   data() {
     return {
       holiday: {
-        date: '',
-        title: ''
+        date: "",
+        title: "",
       },
       holidays: json,
       nextHoliday: undefined,
       palettes: [
         {
-          primary:'#F79272',
-          secondary: '#B589B4',
-          text: '#FBFBFB'
+          primary: "#8447FF",
+          secondary: "#D972FF",
+          text: "#FBFBFB",
         },
         {
-          primary:'#8447FF',
-          secondary: '#D972FF',
-          text: '#FBFBFB'
+          primary: "#1df495",
+          secondary: "#7405a6",
+          text: "#FBFBFB",
         },
         {
-          primary:'#1df495',
-          secondary: '#7405a6',
-          text: '#FBFBFB'
-        },
-        {
-          primary:'#64a1b9',
-          secondary: '#f340dc',
-          text: '#FBFBFB'
-        },
-        {
-          primary:'#3fc9b9',
-          secondary: '#b2f70c ',
-          text: '#FBFBFB'
+          primary: "#64a1b9",
+          secondary: "#f340dc",
+          text: "#FBFBFB",
         },
       ],
-      selectedColor: Math.ceil(((Math.random() * 100) % 5) - 1) 
-
-    }
-  }, 
+      selectedColor: Math.ceil(((Math.random() * 100) % 3) - 1),
+    };
+  },
   beforeMount() {
+    const holidaysList = this.holidays;
+    const current_date = new Date();
 
-      const holidaysList = this.holidays
-      const current_date = new Date()
-
-      this.nextHoliday = holidaysList.find((holiday) => {
-        const date = new Date(holiday.date)
-        return date >= current_date
-      })
+    this.nextHoliday = holidaysList.find((holiday) => {
+      const date = new Date(holiday.date);
+      return date >= current_date;
+    });
+    this.weekday = new Date(this.nextHoliday.date).getUTCDay();
   },
   computed: {
     formattedTitle() {
-      return this.nextHoliday.name
+      return this.nextHoliday.name;
     },
     formattedDate() {
-      return this.formattedDay + ' de ' + this.formattedMonth
+      return this.formattedDay + " de " + this.formattedMonth;
     },
     formattedDay() {
-      console.log('Computed')
-      const day = this.nextHoliday.date.split('-')[2]
+      const day = this.nextHoliday.date.split("-")[2];
       return day;
     },
     formattedMonth() {
@@ -89,26 +88,27 @@ export default {
         "Setembro",
         "Outubro",
         "Novembro",
-        "Dezembro"
-      ]
-      const month = this.nextHoliday.date.split('-')[1]
+        "Dezembro",
+      ];
+      const month = this.nextHoliday.date.split("-")[1];
 
-      return months[parseInt(month) - 1]
+      return months[parseInt(month) - 1];
     },
-    formattedWeekDay() {
-      return [
-        "Domingo",
-        "Segunda-feira",
-        "Terça-feira",
-        "Quarta-feira",
-        "Quinta-feira",
-        "Sexta-feira",
-        "Sábado"
-      ]
-    }
-  }
-}
+    formattedWeekday() {
+      const weekdays = [
+        "domingo",
+        "segunda-feira",
+        "terça-feira",
+        "quarta-feira",
+        "quinta-feira",
+        "sexta-feira",
+        "sábado",
+      ];
 
+      return weekdays[this.weekday];
+    },
+  },
+};
 </script>
 
 <style>
@@ -124,7 +124,8 @@ export default {
   justify-content: center;
 }
 
-body, html {
+body,
+html {
   margin: 0;
   padding: 0;
   height: 100%;
@@ -157,7 +158,7 @@ body, html {
   .container {
     text-align: center;
     justify-content: center;
-    font-size: 0.1rem!important;
+    font-size: 0.1rem !important;
   }
 
   .header {
